e8eb7bbc8edb7216187eb19ac8fcf477a081ceca
Ramitphi <18ucs144@lnmiit.ac.in>
2020-07-17 16:23:39 +0530

    lets deploy

---
+++ bankchurn.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pickle
+import numpy as np
+model  = pickle.load(open('clf.pkl','rb'))
+st.title('Bank Churn Predictio')
+html_temp ="""
+<div style ="background-color:#025246 ;padding:10px">
+<h2 style="color:white;text-align:center;">Bank Churn Prediction ML App</h2>
+</div>
+"""
+st.markdown(html_temp,unsafe_allow_html=True)
+Age =st.text_input("Age","Type Here")
+Tenure =st.text_input("Tenure","Type Here")
+Balance =st.text_input("Balance","Type Here")
+HasCrCard =st.text_input("HasCrCard","Type Here")
+EstimatedSalary =st.text_input("EstimatedSalary","Type Here")
+
+safe_html = """
+<div style ="background-color:#F4D03F;padding:10px>
+<h2 style="color:white; text-align:center;">Customer will not exit</h2>
+</div>
+"""
+danger_html="""
+<div style ="background-color:#F4D03F;padding:10px>
+<h2 style="color:white; text-align:center;">Customer will exit</h2>
+</div>
+"""
+def predict_cust(Age,	Tenure	,Balance,	HasCrCard,EstimatedSalary):
+    input = np.array([[Age,	Tenure	,Balance,	HasCrCard,EstimatedSalary]]).astype(np.float64)
+    prediction=model.predict_proba(input)
+    pred = np.argmax(prediction)
+    return pred
+    
+
+
+
+if st.button("Predict"):
+    output=predict_cust(Age,	Tenure	,Balance,	HasCrCard,EstimatedSalary)
+    st.success("The verdict{}".format(output))
+
+    if output ==0:
+        st.markdown(safe_html,unsafe_allow_html=True)
+    else:
+        st.markdown(danger_html,unsafe_allow_html=True)
+
+
+
